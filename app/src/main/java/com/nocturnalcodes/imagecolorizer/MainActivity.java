dbfef05fb325cd08208da873d04b2df7066502eb
jonas1015 <jonasgsmart1015@gmail.com>
2023-06-19 11:10:00 +0300

    Colorize image initiation

---
+++ app/src/main/java/com/nocturnalcodes/imagecolorizer/MainActivity.java
@@ -0,0 +1,65 @@
+package com.nocturnalcodes.imagecolorizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    int[] imageResIds = {
+      R.drawable.kili1,
+      R.drawable.kili2,
+      R.drawable.kili3,
+    };
+    int imageIndex = 0;
+    boolean color = true;
+    boolean red = true;
+    boolean green = true;
+    boolean blue = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        loadImage();
+    }
+
+    private void loadImage() {
+        Glide.with(this).load(imageResIds[imageIndex]).into(imageView);
+    }
+
+    private void updateSaturation(){
+        ColorMatrix colorMatrix = new ColorMatrix();
+        if(color){
+            red = green = blue = true;
+            colorMatrix.setSaturation(1);
+        } else {
+            colorMatrix.setSaturation(0);
+        }
+
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        imageView.setColorFilter(colorFilter);
+    }
+
+    private void updateColors(){
+        ColorMatrix colorMatrix = new ColorMatrix();
+        float[] matrix = {
+                1, 0, 0, 0, 0,
+                0, 1, 0, 0, 0,
+                0, 0, 1, 0, 0,
+                0, 0, 0, 1, 0,
+        };
+        if(!red) matrix[0] = 0;
+        if(!green) matrix[6] = 0;
+        if(!blue) matrix[12] = 0;
+        colorMatrix.set(matrix);
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        imageView.setColorFilter(colorFilter);
+    }
+}
