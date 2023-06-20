28c9db1910aa09022a0a7cf22ce9fa70c9f6a0a3
jonas1015 <jonasgsmart1015@gmail.com>
2023-06-20 11:03:49 +0300

    Add menu in an app

--- app/src/main/java/com/nocturnalcodes/imagecolorizer/MainActivity.java
+++ app/src/main/java/com/nocturnalcodes/imagecolorizer/MainActivity.java
@@ -2,9 +2,13 @@ package com.nocturnalcodes.imagecolorizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -32,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadImage() {
         Glide.with(this).load(imageResIds[imageIndex]).into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
     }
 
     private void updateSaturation(){
